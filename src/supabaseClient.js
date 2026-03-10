24bbedfa8c5d07f1b0c908f9b5b292a864c42862
Roxana-Maria <42209738+wolfibleah@users.noreply.github.com>
2026-03-11 00:38:23 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://etcsbftrvclakvfwimmo.supabase.co'
-const supabaseKey = 'sb_publishable_j_BtNNffa-6Vbq9mq7QCyg_2LqMHftR'
+const supabaseKey = ''
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
